7c290782f20bf7ea922dd60d9e90a4be9ba74229
Brianna Veenstra <b.g.veenstra@gmail.com>
2018-08-02 18:29:14 -0700

    add Environment to Build model

--- models/circle_v2.go
+++ models/circle_v2.go
@@ -5,9 +5,10 @@ type CircleYamlV2 struct {
 	Version int `yaml:"version,omitempty"`
 	Jobs    struct {
 		Build struct {
-			WorkingDirectory string        `yaml:"working_directory,omitempty"`
-			Docker           []Docker      `yaml:"docker,omitempty"`
-			Steps            []interface{} `yaml:"steps,omitempty"`
+			WorkingDirectory string            `yaml:"working_directory,omitempty"`
+			Docker           []Docker          `yaml:"docker,omitempty"`
+			Environment      map[string]string `yaml:"environment,omitempty"`
+			Steps            []interface{}     `yaml:"steps,omitempty"`
 		} `yaml:"build,omitempty"`
 	} `yaml:"jobs,omitempty"`
 }
